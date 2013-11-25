bb2009f6d8bb6bf1eef3c652bdaf08e2697faef8
root <root@AY1306100114420217d6Z.(none)>
2013-11-26 03:22:11 +0800

    test

--- code/controllers/Adminhtml/UpdatesController.php
+++ code/controllers/Adminhtml/UpdatesController.php
@@ -52,10 +52,10 @@ class CosmoCommerce_Updates_Adminhtml_UpdatesController extends Mage_Adminhtml_C
    
             
             chdir($mod_path);
-            $last_line = exec(escapeshellcmd('git commit -am ').escapeshellarg($note), $output,$retval);
+            //$last_line = exec(escapeshellcmd('git commit -am ').escapeshellarg($note), $output,$retval);
             
             $last_line_commit = exec(escapeshellcmd('git push '.$commiturl), $output_commit,$retval_commit);
-            
+            echo 'git push '.$commiturl;
             //$last_line = exec('git commit -a -m "'.$note.'" ', $output,$retval);
            print_r($output_commit);
            
