85d8ddf12ec63654d12ab3bc5cb2ed25fa5eafba
www-data <www-data@AY1306100114420217d6Z.(none)>
2013-11-26 03:01:06 +0800

    test

--- code/controllers/Adminhtml/UpdatesController.php
+++ code/controllers/Adminhtml/UpdatesController.php
@@ -54,7 +54,7 @@ class CosmoCommerce_Updates_Adminhtml_UpdatesController extends Mage_Adminhtml_C
             chdir($mod_path);
             $last_line = exec(escapeshellcmd('git commit -am ').escapeshellarg($note), $output,$retval);
             
-            $last_line_commit = exec(escapeshellcmd('git push --repo '.$commiturl), $output_commit,$retval_commit);
+            $last_line_commit = exec(escapeshellcmd('git push '.$commiturl), $output_commit,$retval_commit);
             
             
             //$last_line = exec('git commit -a -m "'.$note.'" ', $output,$retval);
